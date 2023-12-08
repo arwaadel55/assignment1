c4189c78b8fe01e6d35118acdb944f59cc0dfbeb
arwa <arwabackendnode@gmail.com>
2023-12-08 19:21:05 +0200

    deploy
    update

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 
 import express  from 'express'
-
-
 import initApp from './src/index.router.js'
-
 const app = express()
 const port =  5000
-
-
-
-initApp(app ,express)
-
-
-
-
-
-  
+initApp(app ,express)  
 app.listen(port, () => console.log(`server is running......... ${port}!`))
